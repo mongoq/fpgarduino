71e58c2e344ae400d22103a1db4d5c9d9b5be453
mongoq <mongoq@das-labor.org>
2018-11-05 18:08:28 +0100

    Add files via upload

---
+++ arduino_example_code/gpio_32_33/gpio_32_33.ino
@@ -0,0 +1,24 @@
+/*
+ Blinklicht ;-)
+ */
+
+int led1 = 32;           // GPIO Pin 7 and 10
+int led2 = 33;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+;}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+  digitalWrite(led2, LOW);
+  delay(1000);
+}
